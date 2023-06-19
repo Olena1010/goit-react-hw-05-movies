c40f999a96b2578d1dcd22b23b8062db0e454da5
Olena1010 <ebojcenko55@gmail.com>
2023-06-19 17:54:29 +0300

    add pages

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { App } from 'components/App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  
   <React.StrictMode basename="/goit-react-hw-05-movies">
     <BrowserRouter>
       <App />
@@ -12,3 +13,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
  
+
+// 908e7ab925fbf0105bff462a17b34666
+// eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MDhlN2FiOTI1ZmJmMDEwNWJmZjQ2MmExN2IzNDY2NiIsInN1YiI6IjY0OTA2MjdkNDJiZjAxMDEzYmJlNTg2MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.ejm5M2odP6Ads3vtJjJvpgxfgeW_VCujfiZKUiSYKF8
