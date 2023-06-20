1f814ee9c8b5f6c2e031a1dfe3c7bd49c25a1004
Olena1010 <ebojcenko55@gmail.com>
2023-06-20 17:40:15 +0300

    add notFoundPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import { SharedLayout } from './index';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
+const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'))
 
 export const App = () => {
   return (
@@ -13,8 +14,8 @@ export const App = () => {
         <Route path="movies/:movieId" element={<MovieDetailsPage />}>
           <Route path="cast" element={<CastPage />} />
           <Route path="reviews" element={<ReviewsPage />} />
-        </Route>
-        <Route path="*" element={<NotFoundPage />} /> */}
+        </Route> */}
+        <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
   );
--- src/components/Container/Container.jsx
+++ src/components/Container/Container.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 import { AppWrapper } from './Container.styled';
+
+
 const Container = ({ children }) => {
     return <AppWrapper>{children}</AppWrapper>;
 };
