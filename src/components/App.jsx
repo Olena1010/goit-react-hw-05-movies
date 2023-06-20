75f68404a4cbd1a9e6b9687edb7efe398068c6b0
Olena1010 <ebojcenko55@gmail.com>
2023-06-20 19:35:53 +0300

    add movieDetailsPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,6 +4,9 @@ import { SharedLayout } from './index';
 
 const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const MoviesPage = lazy(() => import('../pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() =>
+  import('../pages/MovieDetailsPage/MovieDetailsPage')
+);
 const NotFoundPage = lazy(() => import('../pages/NotFoundPage/NotFoundPage'));
 
 
@@ -13,10 +16,10 @@ export const App = () => {
       <Route path="/" element={<SharedLayout />}>
         <Route index element={<HomePage />} />
         <Route path="movies" element={<MoviesPage />} />
-        {/* <Route path="movies/:movieId" element={<MovieDetailsPage />}>
-          <Route path="cast" element={<CastPage />} />
-          <Route path="reviews" element={<ReviewsPage />} />
-        </Route> */}
+        <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+          {/* <Route path="cast" element={<CastPage />} />
+          <Route path="reviews" element={<ReviewsPage />} /> */}
+        </Route>
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
--- src/components/ButtonLink/ButtonLink.styled.jsx
+++ src/components/ButtonLink/ButtonLink.styled.jsx
@@ -9,14 +9,14 @@ export const NavButton = styled(NavLink)`
   display: inline-block;
   min-width: 95px;
   padding: 4px 8px;
-  margin-bottom: 50px;
+  // margin-bottom: 50px;
   text-align: center;
   font-size: 30px;
   font-weight: 700;
   letter-spacing: 2px;
   border-radius: 5px;
   color: #ffffff;
-  background-color: #4682b4;
+  background-color: rgba(3, 37, 65);
   transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
   &:hover {
     box-shadow: 0px 0 11px 1px rgb(0 0 0 / 90%);
