f099bba374dfb397d53695500540a8c285034e4c
Olena1010 <ebojcenko55@gmail.com>
2023-06-22 19:39:56 +0300

    replaced favicon

--- src/components/MoviesGallery/MoviesGallery.styled.jsx
+++ src/components/MoviesGallery/MoviesGallery.styled.jsx
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 export const MoviesList = styled.ul`
   display: grid;
   max-width: calc(120vw - 48px);
-
   grid-gap: 16px;
   @media screen and (min-width: 760px) {
     grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
