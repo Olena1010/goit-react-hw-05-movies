b6b1aa7cb46561b7d91b419ba386310029e5f934
Olena1010 <ebojcenko55@gmail.com>
2023-06-22 20:16:02 +0300

    added edits

--- src/components/PageHeading/PageHeading.styled.jsx
+++ src/components/PageHeading/PageHeading.styled.jsx
@@ -3,6 +3,9 @@ import styled from '@emotion/styled';
 export const PageTitle = styled.h1`
 padding: 50px 0;
 font-weight: 700;
+text-shadow: 1px 0 11px rgb(255 0 0 / 90%);
+font-size: 30px;
+
  @media screen and (min-width: 760px) {  
   padding: 30px 0;
   line-height: 58px;
@@ -11,4 +14,5 @@ font-weight: 700;
   font-size: 50px;
   color: #2a2a2a;
   font-weight: 800;
+ }
 `;
